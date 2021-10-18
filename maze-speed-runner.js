cd628e4133454cbaf09a5612fc89261831fcbd8a
AVIV POLAK <avivp0437@gmail.com>
2021-10-18 20:54:21 +0300

    good comit name

--- maze-speed-runner.js
+++ maze-speed-runner.js
@@ -1,49 +1,52 @@
+import { readFileSync, readdirSync } from "fs";
 function openChestSync(chestPath) {
-  let chestContentJSON = "";
-  try {
-    let chestContent = fs.readFileSync(chestPath);
-    chestContentJSON = JSON.stringify(chestContent.toString());
-  } catch (err) {
-    return false;
-  }
-  if (validateChestContent(chestContentJSON)) {
-    return chestContentJSON;
-  } else {
-    return false;
-  }
+    let chestContentJSON = "";
+    try {
+        let chestContent = readFileSync(chestPath);
+        console.log(chestContent);
+        chestContentJSON = JSON.stringify(chestContent.toString());
+    } catch (err) {
+        return false;
+    }
+    if (validateChestContent(chestContentJSON)) {
+        return chestContentJSON;
+    } else {
+        return false;
+    }
 }
 
+console.log(openChestSync("./maze/room-1/room-0"));
 function validateChestContent(chestText) {
-  try {
-    let chestContent = fs.readFileSync(JSON.parse(chestText).clue);
-  } catch (error) {
-    return false;
-  }
-  return true;
+    try {
+        let chestContent = readFileSync(JSON.parse(chestText).clue);
+    } catch (error) {
+        return false;
+    }
+    return true;
 }
 
 function findTreasureSync(roomPath) {
-  let mazeArray = fs.readdirSync(roomPath);
-  if (isTreasure(openChestSync(roomPath))) {
-    return "FOUND";
-  }
-  for (let elem of mazeArray) {
-    //console.log(elem);
-    if (openChestSync("./maze/" + elem)) {
-      findTreasureSync(openChestSync("./maze/" + elem));
+    let mazeArray = readdirSync(roomPath);
+    if (isTreasure(openChestSync(roomPath))) {
+        return "FOUND";
     }
-  }
+    for (let elem of mazeArray) {
+        //console.log(elem);
+        if (openChestSync("./maze/" + elem)) {
+            findTreasureSync(openChestSync("./maze/" + elem));
+        }
+    }
 }
 
 function isTreasure(content) {
-  if (JSON.parse(content).hasOwnProperty("treasure")) {
-    return true;
-  }
-  return false;
+    if (JSON.parse(content).hasOwnProperty("treasure")) {
+        return true;
+    }
+    return false;
 }
 
 function drawMapSync(currentRoomPath) {}
 
-//console.log(findTreasureSync("./maze"));
-console.log(openChestSync("./maze/chest-1.json"));
-console.log(validateChestContent("./maze/room-1/room-0"));
+// //console.log(findTreasureSync("./maze"));
+// console.log(openChestSync("./maze/chest-1.json"));
+// console.log(validateChestContent("./maze/room-1/room-0"));
